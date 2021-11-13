80ccf64c9c4b359ef298ff4bc7f64f1a7276ef18
Kristy <kristyd463@gmail.com>
2021-11-13 10:52:25 +1000

    pushing to heroku, created base pages

---
+++ flat-byte/src/Pages/CarDetailPage.jsx
@@ -0,0 +1,7 @@
+// import React from "react";
+
+// function CarDetailPage() {
+//   return <h1>This is the project page.</h1>;
+// }
+
+// export default CarDetailPage;
---
+++ flat-byte/src/Pages/HomePage.jsx
@@ -0,0 +1,7 @@
+// import React, { useState, useEffect } from "react";
+
+// function HomePage() {
+//   return <h1>This is the home page.</h1>;
+// }
+
+// export default HomePage;
