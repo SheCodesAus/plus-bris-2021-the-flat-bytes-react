870c8c14030c8a13e888d73ea0d9149d82e22eb4
iusharnina@gmail.com <zjulia679@gmail.com>
2021-11-19 17:10:56 +1000

    Fixed filtering

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -3,11 +3,12 @@ import "./CarCard.css";
 
 function CarCard(props) {
   const { ...suggestedCar } = props;
+  const {...bestMatchCar} = props;
   return (
     <div className="car-card">
       <img alt="" src={suggestedCar.image} />
       <h3>This your best choice carcard</h3>
-      <h3>Car make: {suggestedCar.make}</h3>
+      <h3>Car make: {<h3>Car make: {suggestedCar.make}</h3>}</h3>
       <h3>Car model: {suggestedCar.model}</h3>
       <h3>Car price: $ {suggestedCar.price}</h3>
       <h3>Car price: {suggestedCar.colour}</h3>
