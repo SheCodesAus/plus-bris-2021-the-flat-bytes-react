fbbf0c1af5ed70b4cbda9053eaf24cf7a796463f
qambatali <q.ambatali@gmail.com>
2021-11-20 12:28:03 +1000

    Update NotFound.jsx

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const PageNotFound = () => {
   return (
       <div>
+      <img id="banner" src={"../Luxe-logo-banner.png"} alt=""/>
               <h2>This page could not be found</h2>
       </div >
   )
