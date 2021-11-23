348ba43929b92909b120a039e1e9e178506f59ff
Dalila K Leo <dalilakatialeo@gmail.com>
2021-11-23 12:51:25 +1000

    upgrades on buttons

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -229,16 +229,20 @@ function Form() {
     return result;
   }
 
-  console.log("cars options", carsOptions);
+  // console.log("cars options", carsOptions);
 
   return (
-    <div>
+    <div id="form-page">
       <div id="img-div">
         <img id="banner-small" src={"../Luxe-logo-banner.png"} alt="" />
       </div>
+      <div id="form-wrapper">
       <form onSubmit={handleSubmit}>
         <div class="container">
-          <label class="standard-text">
+          <div class="form-intro">
+            <p>Answer these few questions and our AI will determine <br/> the best purchase for you</p>
+          </div>
+          <label class="form-input standard-text">
             How much money are you willing to spend?
           </label>
           <select onClick={getUserPreferences} id="price" name="price">
@@ -249,7 +253,7 @@ function Form() {
           </select>
         </div>
         <div class="container">
-          <label class="standard-text">What color do you prefer?</label>
+          <label class="form-input standard-text">What color do you prefer?</label>
           <select onChange={getUserPreferences} id="colour" name="colour">
             <option disabled selected value=""></option>
             <option value="black">black</option>
@@ -260,7 +264,7 @@ function Form() {
         </div>
 
         <div class="container">
-          <label class="standard-text">
+          <label class="form-input standard-text">
             In what body type do you feel most comfortable?
           </label>
           <select onChange={getUserPreferences} id="body_type" name="body_type">
@@ -273,7 +277,7 @@ function Form() {
         </div>
 
         <div class="container">
-          <label class="standard-text">Do you love nature?</label>
+          <label class="form-input standard-text">Do you love nature?</label>
           <select onChange={getUserPreferences} id="fuel" name="fuel">
             <option disabled selected value=""></option>
             <option value="electric">Yes, of course</option>
@@ -282,7 +286,7 @@ function Form() {
         </div>
 
         <div class="container">
-          <label class="standard-text">How are you feeling today?</label>
+          <label class="form-input standard-text">How are you feeling today?</label>
           <select
             value={mood}
             onChange={(e) => setMood(e.target.value)}
@@ -298,7 +302,7 @@ function Form() {
               It could be better
             </option>
             <option onClick={handleMoodQuestion} value="tired">
-              I am tried and not in the mood today
+              I am tired and not in the mood today
             </option>
             <option onClick={handleMoodQuestion} value="cheerful">
               I am smiling and happy today!
@@ -309,6 +313,7 @@ function Form() {
           <button type="submit">Help me choose</button>
         </div>
       </form>
+      </div>
       <h1 class="standard-text">Best Car Matches:</h1>
       {!bestCarMatches.length && (
         <p class="standard-text">No car matches found.</p>
