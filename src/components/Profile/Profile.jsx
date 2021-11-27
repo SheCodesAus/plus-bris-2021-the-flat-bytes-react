c7773194bbb8744b252daca4a3b3ff577d1983b1
Kristy <kristyd463@gmail.com>
2021-11-27 10:59:43 +1000

    fix banner on profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -21,34 +21,31 @@ const Profile = () => {
   return (
     <div>
       <div>
-        <div>
-          {/* <input type="text" value={name} onChange={handleChange} />
-          {/* <button type="button" onClick={handleAdd}>
+        {/*/<div>
+           <input type="text" value={name} onChange={handleChange} />
+          <button type="button" onClick={handleAdd}>
             Add
-          </button> */}
-          {/* <div>{Preferences}</div> */}
+          </button>
+          <div>{Preferences}</div>
         </div>
       </div>
       <ul>
         {Preferences.map((item) => (
           <li key={item.id}>{item.name}</li>
         ))}
-      </ul>
-      );
-      <div id="img-div">
-        <img id="banner" src={"../Luxe-logo-banner.png"} alt="" />
-      </div>
-      <h3 class="standard-text">Welcome to your page.</h3>
-      <h3 class="standard-text">
-        These are your favourites from our selection tailored just for you...
-      </h3>
-      <div>{Preferences}</div> Todo:link up to favourite function
-      <h3 class="standard-text"> More to love:</h3>
-      <div class="container">
-        <button class="profile-button"> Jewellery </button>
-        <button class="profile-button"> Holiday Homes </button>
-        <button class="profile-button"> Yachts </button>
-        <button class="profile-button"> Private Jets </button>
+      </ul> */}
+
+        <h3 class="standard-text">Welcome to your page.</h3>
+        <h3 class="standard-text">
+          These are your favourites from our selection tailored just for you...
+        </h3>
+        <h3 class="standard-text"> More to love:</h3>
+        <div class="container">
+          <button class="profile-button"> Jewellery </button>
+          <button class="profile-button"> Holiday Homes </button>
+          <button class="profile-button"> Yachts </button>
+          <button class="profile-button"> Private Jets </button>
+        </div>
       </div>
     </div>
   );
