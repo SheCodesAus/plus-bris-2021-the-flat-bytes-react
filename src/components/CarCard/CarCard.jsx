81cf78d7fab5355ec9e38a91c85b15a65a34cb74
qambatali <q.ambatali@gmail.com>
2021-12-03 00:02:04 +1000

    car card tweeks

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -14,18 +14,24 @@ function CarCard({ image, make, car_model, price, colour, body_type, url }) {
     <div className="car-card">
       <img alt="" src={image} />
       <div class="car-details">
+
+
+
+      <ul><h3>{make} - {car_model}</h3></ul>
       <ul>
-      <h3>{make} - {car_model}</h3></ul>
       <li><b>Price</b>$ {price}</li>
       <li><b>Color</b>{colour}</li>
       <li><b>Body Type</b>{body_type}</li>
       
-      <li><h4>Additional Details:<br /> {" "}
+      <li><b>Additional Details:</b>{" "}
         <a href={url} target="_blank">
           {" "}
           Open in new window
-        </a><br /></h4></li></div>
+        </a><br /></li></ul>
+        
+        </div>
       <div>
+        <br />
         <button type="submit" onClick={saveFavourite}>
           Save Favourite
         </button>
